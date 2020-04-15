a0ca0c744cc7ed2d95a64ca07cc7c0a7a5dec0cc
penghuaping <penghuaping@huobi.com>
2020-04-15 17:43:29 +0800

    修改节点名

--- docker/run.sh
+++ docker/run.sh
@@ -9,13 +9,13 @@ WORK_DIR=$2
 #consul的目录
 CONSUL_DIR=$3
 #应用程序名
-APPLICATION_NAME=$4
+APP_NAME=$4
 #应用程序版本号
-APPLICATION_VERSION=$5
+APP_VERSION=$5
 #应用程序包名
-APPLICATION_PACKAGE_NAME=${APPLICATION_NAME}-${APPLICATION_VERSION}.jar
+APP_PACKAGE=${APP_NAME}-${APP_VERSION}.jar
 #节点名
-NODE_NAME=${APPLICATION_NAME}-$(/bin/hostname)
+NODE_NAME=${APP_NAME}-$(hostname)
 #进程文件
 NODE_PIDFILE=${WORK_DIR}/consul.pid
 #网卡名
@@ -24,7 +24,7 @@ ADDR=eth0
 LOCAL_IP=127.0.0.1
 
 #获取容器IP
-NODE_IP=$(/sbin/ifconfig ${ADDR} | /bin/grep inet | /bin/grep -v ${LOCAL_IP} | /bin/grep -v inet6 | /usr/bin/awk '{print $2}' | /usr/bin/tr -d "addr:")
+NODE_IP=$(ifconfig ${ADDR} | grep inet | grep -v ${LOCAL_IP} | grep -v inet6 | awk '{print $2}' | tr -d "addr:")
 
 #启动consul
 # 只允许当前ip注册，即只允许该容器内的应用程序注册到该consul节点
@@ -38,15 +38,15 @@ nohup ${CONSUL_BIN} agent \
 -node=${NODE_NAME} \
 -rejoin -retry-interval=30s \
 -data-dir=${CONSUL_DIR}/data \
--log-file=${CONSUL_DIR}/log/${APPLICATION_NAME}.log -log-level=INFO \
+-log-file=${CONSUL_DIR}/log/${APP_NAME}.log -log-level=INFO \
 -retry-join=172.18.18.141 -retry-join=172.18.18.142 -retry-join=172.18.18.143 -retry-join=172.18.18.144 -retry-join=172.18.18.145 -retry-join=172.18.18.146 \
 -join=172.18.18.141 -join=172.18.18.142 -join=172.18.18.143 -join=172.18.18.144 -join=172.18.18.145 -join=172.18.18.146 \
 > /dev/null 2>&1 &
 
 #启动应用程序
-/usr/bin/java -jar  \
+java -jar  \
 -DCONSUL_NODES=127.0.0.1:8500,172.18.18.141:8500,172.18.18.142:8500,172.18.18.143:8500,172.18.18.144:8500,172.18.18.145:8500,172.18.18.146:8500 \
-${APPLICATION_PACKAGE_NAME} \
+${APP_PACKAGE} \
 > /dev/null 2>&1 &
 
 #保持容器不退出
