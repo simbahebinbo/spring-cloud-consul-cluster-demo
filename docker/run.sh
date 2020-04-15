939e82731d440a53f4ceabc3ad8fb633eeeac30f
penghuaping <penghuaping@huobi.com>
2020-04-15 23:55:04 +0800

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -16,8 +16,6 @@ APP_VERSION=$5
 APP_PACKAGE=${APP_NAME}-${APP_VERSION}.jar
 #节点名
 NODE_NAME=${APP_NAME}-$(hostname)
-#进程文件
-NODE_PIDFILE=${WORK_DIR}/consul.pid
 #网卡名
 ADDR=eth0
 #本地IP
@@ -30,24 +28,18 @@ NODE_IP=$(ifconfig ${ADDR} | grep inet | grep -v ${LOCAL_IP} | grep -v inet6 | a
 # 只允许当前ip注册，即只允许该容器内的应用程序注册到该consul节点
 # 以 client 模式启动
 # 加入到 server 模式 的节点组成的集群
-nohup ${CONSUL_BIN} agent \
--pid-file=${NODE_PIDFILE} \
+${CONSUL_BIN} agent \
 -bootstrap-expect=0 \
 -advertise=${NODE_IP} -bind=${NODE_IP} -client=${LOCAL_IP} \
--protocol=3 \
 -node=${NODE_NAME} \
--rejoin -retry-interval=30s \
 -data-dir=${CONSUL_DIR}/data \
 -log-file=${CONSUL_DIR}/log/${APP_NAME}.log -log-level=INFO \
--retry-join=172.18.18.141 -retry-join=172.18.18.142 -retry-join=172.18.18.143 -retry-join=172.18.18.144 -retry-join=172.18.18.145 -retry-join=172.18.18.146 \
--join=172.18.18.141 -join=172.18.18.142 -join=172.18.18.143 -join=172.18.18.144 -join=172.18.18.145 -join=172.18.18.146 \
-> /dev/null 2>&1 &
+-join=172.18.18.141 -join=172.18.18.142 -join=172.18.18.143 -join=172.18.18.144 -join=172.18.18.145 -join=172.18.18.146
 
 #启动应用程序
 java -jar  \
 -DCONSUL_NODES=127.0.0.1:8500,172.18.18.141:8500,172.18.18.142:8500,172.18.18.143:8500,172.18.18.144:8500,172.18.18.145:8500,172.18.18.146:8500 \
-${APP_PACKAGE} \
-> /dev/null 2>&1 &
+${APP_PACKAGE}
 
 #保持容器不退出
 while true;do
